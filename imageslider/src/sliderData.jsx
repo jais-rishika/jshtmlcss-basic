ea048011c7b103408e103d1372dd3837dc0fafbc
Rishika Jaiswal <jaisrishika@gmail.com>
2024-01-07 21:28:40 +0530

    imageSlider project added

---
+++ imageslider/src/sliderData.jsx
@@ -0,0 +1,19 @@
+const slides=[
+    {
+        "alt":1,
+        "image": "1.jpeg"
+    },
+    {
+        "alt":2,
+        "image": "2.jpeg"
+    },
+    {
+        "alt":3,
+        "image": "3.jpeg"
+    },
+    {
+        "alt":4,
+        "image": "4.jpeg"
+    }
+]
+export default slides;
